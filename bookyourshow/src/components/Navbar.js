1d13f7babfa87dd8c2d6afdf4dda7a34b2813e4e
somil panwar <122174329+somilpanwar@users.noreply.github.com>
2024-08-11 13:13:24 +0530

    navbar added

---
+++ bookyourshow/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../style/allcss.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      nav bar
+    </div>
+  )
+}
+
+export default Navbar
