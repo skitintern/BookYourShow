ed2f5f8dd4a2fec6236e6fd5dd5bd58dba00467d
somil panwar <122174329+somilpanwar@users.noreply.github.com>
2024-08-12 11:58:20 +0530

    add movie page

--- bookyourshow/src/App.jsx
+++ bookyourshow/src/App.jsx
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import Movies from './components/Movies';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
 
     <Navbar/>
+    <Movies/>
     </>
     );
 }
